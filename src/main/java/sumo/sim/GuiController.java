10f83f8e16f7e7e347ea885dc3fe3e6e95918cd8
LeleZss <liuzzoleandro@gmail.com>
2025-11-29 19:42:48 +0100

    added css file and updated xml file

--- src/main/java/sumo/sim/GuiController.java
+++ src/main/java/sumo/sim/GuiController.java
@@ -1,14 +1,71 @@
 package sumo.sim;
 
+import javafx.animation.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class GuiController {
+    // attributes from FXML to communicate with objects
     @FXML
-    private Label welcomeText;
+    public AnchorPane addMenue;
+    @FXML
+    private ToggleButton playButton;
+    @FXML
+    private ToggleButton selectButton;
+    @FXML
+    private ToggleButton addButton;
+    @FXML
+    private Label timeLabel;
+    @FXML
+    private Spinner <Integer> delaySelect;
+    @FXML
+    private VBox selectMenue;
 
-    @FXML // example for button execution, doesnt do anything
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML
+    public void initialize() {
+        timeLabel.setText("00:00");
+        SpinnerValueFactory<Integer> valueFactory = // manages spinner
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 500, 50); //min,max, start
+        delaySelect.setValueFactory(valueFactory);
+
     }
+
+    @FXML
+    protected void onPlayStart() {
+        if (playButton.isSelected()) { // toggled
+            System.out.println("Started");
+        } else {
+            System.out.println("Stopped");
+        }
+    }
+
+    @FXML
+    protected void onSelect(){
+        if (selectButton.isSelected()) { // toggled
+            System.out.println("Started");
+        } else {
+            System.out.println("Stopped");
+        }
+    }
+
+    @FXML
+    protected void onAdd(){ // experimental animation
+        FadeTransition fade = new FadeTransition(Duration.millis(200), addMenue);
+        if (addButton.isSelected()) { // toggled
+            addMenue.setVisible(true);
+            fade.setFromValue(0);
+            fade.setToValue(1);
+            fade.play();
+        } else {
+            fade.setFromValue(1);
+            fade.setToValue(0);
+            fade.play();
+            addMenue.setVisible(false);
+        }
+    }
+
 }
+
